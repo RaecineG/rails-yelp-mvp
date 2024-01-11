90eb4e50c3f93733bed6e465f9d19230c2406dcc
Raecine Green <raecine_g@hotmail.com>
2024-01-11 19:04:46 +0900

    restaurants user story completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  Rails.application.routes.draw do
+    resources :restaurants, only: [:index, :new, :create, :show] do
+      resources :reviews, only: [:new, :create]
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Clear existing records
+puts "Cleaning Database..."
+Restaurant.destroy_all
+
+# Seed new records
+puts "Creating restaurants ..."
+restaurants_data = [
+  { name: 'Comme Chez soi', address: 'Lier St', phone_number: '555-1234', category: 'belgian' },
+  { name: 'Fromage', address: 'Jamaica Ave', phone_number: '555-5678', category: 'french' },
+  { name: 'The Red Dragon', address: '789 Pine St', phone_number: '555-9876', category: 'chinese' },
+  { name: 'Pizza Pasta', address: '321 Elm St', phone_number: '555-4321', category: 'italian' },
+  { name: 'Fugu Venom', address: '654 Maple St', phone_number: '555-8765', category: 'japanese' }
+]
+
+restaurants_data.each do |restaurant_data|
+  Restaurant.create!(restaurant_data)
+end
+
+puts "Finished!"
